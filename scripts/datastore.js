0587e3bbb0bc2658b475a244a0aab9c2734a9177
layla-p <layla.porter@gmail.com>
2018-07-25 16:45:04 -0700

    updated local datastore to support promises

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,23 +1,33 @@
 (function (window) {
     'use strict';
     var App = window.App || {};
+    var Promise = window.Promise;
 
     function Datastore() {
         this.data = {};
     }
 
-    Datastore.prototype.add = function(key, value){
-        this.data[key] = value;
-    }
-    Datastore.prototype.get = function(key){
-      return this.data[key];
-    }
-    Datastore.prototype.getAll = function(){
-        return this.data;
+    function promiseResolvedWith(value) {
+        var promise = new Promise(function (resolve, reject) {
+            resolve(value);
+        });
+        return promise;
     }
 
-    Datastore.prototype.remove = function(key){
-       delete this.data[key];
+    Datastore.prototype.add = function (key, value) {
+        this.data[key] = value;
+        return promiseResolvedWith(null);
+    }
+    Datastore.prototype.get = function (key) {        
+        return promiseResolvedWith(this.data[key]);
+    }
+    Datastore.prototype.getAll = function () {
+        return promiseResolvedWith(this.data);
+    }
+
+    Datastore.prototype.remove = function (key) {
+        delete this.data[key];
+        return promiseResolvedWith(null);
     }
 
     App.Datastore = Datastore;
--- scripts/main.js
+++ scripts/main.js
@@ -7,20 +7,21 @@ var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
 
 var App = window.App;
 var Truck = App.Truck;
-//var Datastore = App.Datastore;
+var Datastore = App.Datastore;
 var RemoteDataStore = App.RemoteDataStore;
 var FormHandler = App.FormHandler;
 var Validation = App.Validation;
 var Checklist = App.Checklist;
-var remoteDs = new RemoteDataStore(SERVER_URL);
-// var myTruck = new Truck('867', new Datastore());
-var myTruck = new Truck('867', remoteDs);
+//var remoteDs = new RemoteDataStore(SERVER_URL);
+var myTruck = new Truck('867', new Datastore());
+//var myTruck = new Truck('867', remoteDs);
 window.myTruck = myTruck;
 var formHandler = new FormHandler(FORM_SELECTOR);
 var checklist = new Checklist(CHECKLIST_SELECTOR);
 checklist.addClickHandler(myTruck.deliverOrder.bind(myTruck));
 
 formHandler.addSubmitHandler(function(data){
+    console.log(myTruck.createOrder(data));
     return myTruck.createOrder(data).then(function(){
         checklist.addRow(data);
     });   
